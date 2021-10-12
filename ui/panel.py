6188ddc64f9e1f69c829ed49d2bd337fe02b7496
atticus-lv <64118610+atticus-lv@users.noreply.github.com>
2021-10-13 01:39:49 +0800

    update ui

--- ui/panel.py
+++ ui/panel.py
@@ -8,6 +8,7 @@ from ..ui.icon_utils import BatchPreview
 
 bat_preview = BatchPreview('.bip')
 
+
 def get_pref():
     """get preferences of this plugin"""
     return bpy.context.preferences.addons.get(__folder_name__).preferences
@@ -22,6 +23,13 @@ class SidebarSetup:
     @classmethod
     def poll(self, context):
         return get_pref().use_workflow_panel is False
+
+    def draw(self, context):
+        layout = self.layout
+        self.draw_ui(context, layout)
+
+    def draw_ui(self, context, layout):
+        pass
 
 
 class ADJT_PT_UnitPanel(SidebarSetup, bpy.types.Panel):
@@ -38,12 +46,10 @@ class ADJT_PT_UnitPanel(SidebarSetup, bpy.types.Panel):
         layout.alert = True
         layout.label(text='Scene scale is not optimal', icon='ERROR')
 
-    def draw(self, context):
-        layout = self.layout
-
+    def draw_ui(self, context, layout):
         layout.scale_y = 1.25
         row = layout.row(align=1)
-        row.alignment='CENTER'
+        row.alignment = 'CENTER'
         row.separator()
         row.operator('wm.adjt_set_units', icon_value=bat_preview.get_icon('set_unit'))
 
@@ -51,16 +57,14 @@ class ADJT_PT_UnitPanel(SidebarSetup, bpy.types.Panel):
 class ADJT_PT_CurvePanel(SidebarSetup, bpy.types.Panel):
     bl_label = 'Curve and Mesh'
 
-    def draw(self, context):
-        layout = self.layout.column(align=True)
-
+    def draw_ui(self, context, layout):
         box = layout.box()
-        box.label(text='Curve & Mesh', icon_value=bat_preview.get_icon('extract_curve'))
-        box.operator('curve.adjt_extract_edge_as_curve', icon_value=bat_preview.get_icon('extract_curve'))
+        box.label(text='Mesh', icon_value=bat_preview.get_icon('extract_curve'))
+        box.operator('mesh.adjt_extract_edge_as_curve', icon_value=bat_preview.get_icon('extract_curve'))
         box.operator('mesh.adjt_procedural_keep', icon_value=bat_preview.get_icon('delete'))
 
         box = layout.box()
-        box.label(text='Flow Mesh', icon_value=bat_preview.get_icon('flow'))
+        box.label(text='Flow',     icon_value=bat_preview.get_icon('flow'))
         box.operator('curve.adjt_offset_curve_by_length', icon_value=bat_preview.get_icon('offset_curve'))
         box.operator('curve.adjt_flow_mesh_along_curve', icon_value=bat_preview.get_icon('flow'))
         box.operator('curve.adjt_split_curve_and_flow_mesh', icon_value=bat_preview.get_icon('split'))
@@ -69,15 +73,13 @@ class ADJT_PT_CurvePanel(SidebarSetup, bpy.types.Panel):
 class ADJT_PT_AlignPanel(SidebarSetup, bpy.types.Panel):
     bl_label = 'Geo Tools'
 
-    def draw(self, context):
-        layout = self.layout.column(align=True)
+    def draw_ui(self, context, layout):
+        box = layout.box()
+        box.label(text='Instance', icon_value=bat_preview.get_icon('join'))
+        box.operator("node.adjt_join_geo")
 
         box = layout.box()
-        box.label(text='Instance', icon_value=bat_preview.get_icon('align2'))
-        box.operator("node.adjt_join_geo", icon_value=bat_preview.get_icon('join'))
-
-        box = layout.box()
-        box.label(text='Align', icon_value=bat_preview.get_icon('expose'))
+        box.label(text='Align', icon_value=bat_preview.get_icon('align2'))
 
         # select the instance will not show the preset thumbnails
         if not (context.active_object and context.active_object.name.startswith('ADJT_Render')):
@@ -89,7 +91,7 @@ class ADJT_PT_AlignPanel(SidebarSetup, bpy.types.Panel):
                 p = item.thumbnails[:-4]
 
                 col.label(text=p)
-                box.operator('node.adjt_view_align', icon_value=bat_preview.get_icon('place'), text='Align').node_group_name = p
+                box.operator('node.adjt_view_align', text='Align').node_group_name = p
         else:
             mod = None
             for m in context.active_object.modifiers:
@@ -114,16 +116,16 @@ class ADJT_PT_MeasurePanel(SidebarSetup, bpy.types.Panel):
     bl_label = 'Measure'
     bl_options = {'DRAW_BOX', 'DEFAULT_CLOSED'}
 
-    def draw(self, context):
-        layout = self.layout
+    def draw_ui(self, context, layout):
         box = layout.box()
-        box.operator('mesh.adjt_check_weight', icon_value=bat_preview.get_icon('weight2'))
+        box.label(text='Weighting', icon_value=bat_preview.get_icon('weight2'))
+        box.operator('mesh.adjt_check_weight', )
 
         box = layout.box()
-        box.label(text='Measure', icon='ARROW_LEFTRIGHT')
+        box.label(text='Measure', icon_value=bat_preview.get_icon('measure'))
 
         if not (context.active_object and context.active_object.name.startswith('ADJT_Measure')):
-            box.operator('adjt.measure_bind', icon_value=bat_preview.get_icon('measure'))
+            box.operator('adjt.measure_bind', )
         else:
             mod = None
             for m in context.active_object.modifiers:
@@ -143,9 +145,9 @@ class ADJT_PT_UtilityPanel(SidebarSetup, bpy.types.Panel):
     bl_label = 'Utility'
     bl_options = {'DRAW_BOX', 'DEFAULT_CLOSED'}
 
-    def draw(self, context):
+    def draw_ui(self, context, layout):
         pref = get_pref()
-        box = self.layout.box()
+        box = layout.box()
 
         box.operator('wm.adjt_batch_rename', icon='FONT_DATA')
         box.operator('adjt.init_thumb', icon='BLENDER')
@@ -218,12 +220,11 @@ class ADJT_PT_RenderPanel(SidebarSetup, bpy.types.Panel):
     bl_label = 'Render'
     bl_options = {'DRAW_BOX', 'DEFAULT_CLOSED'}
 
-    def draw(self, context):
-        layout = self.layout
+    def draw_ui(self, context, layout):
 
         box = layout.box()
-        box.label(text='Camera', icon_value=bat_preview.get_icon('render'))
-        box.operator('render.adjt_cam_frame', icon_value=bat_preview.get_icon('frame'))
+        box.label(text='Camera', icon_value=bat_preview.get_icon('frame'))
+        box.operator('render.adjt_cam_frame')
 
         if context.active_object and context.active_object.type == 'CAMERA':
             cam = context.active_object
@@ -236,7 +237,7 @@ class ADJT_PT_RenderPanel(SidebarSetup, bpy.types.Panel):
         box = layout.box()
         row = box.row(align=True)
         row.label(text='Lighting', icon_value=bat_preview.get_icon('light'))
-        row.operator("preferences.studiolight_show", text="", icon="PREFERENCES",emboss=False)
+        row.operator("preferences.studiolight_show", text="", icon="PREFERENCES", emboss=False)
 
         view = context.space_data
         shading = view.shading if view.type == 'VIEW_3D' else context.scene.display.shading
@@ -324,29 +325,54 @@ class ADJT_PT_Workflow(SidebarSetup, bpy.types.Panel):
 
     def draw(self, context):
         layout = self.layout
+        row = layout.row(align=False)
+
+        enum_icons = [
+
+            ('MODEL', 'Model', '', 'place', 0),
+            ('PLACE', 'Place', '', 'join', 1),
+            ('RENDER', 'Render', '', 'render', 2),
+            ('MEASURE', 'Measure', '', 'measure', 3),
+            ('TOOLS', 'Tools', '', 'rename', 4),
+        ]
+
         wm = context.window_manager
-        row = layout.row(align=True)
+        curr = wm.adjt_workflow
+        col = layout.column()
+
+        row = col.row(align=False)
+        row.prop(wm, 'adjt_workflow', expand=True)
+
+        row = col.row(align=False)
         row.scale_y = 1.5
-        row.scale_x = 1.5
-        row.prop(wm, 'adjt_workflow',expand=True)
+        row.scale_x = 1.1
+        for e in enum_icons:
+            if curr == e[0]:
+                box = row.box()
+                box.template_icon(bat_preview.get_icon(e[3]), scale=1.5)
+
+            else:
+                row.template_icon(bat_preview.get_icon(e[3] + '_dark'), scale=1.15)
+
 
         if wm.adjt_workflow == 'MODEL':
-            ADJT_PT_CurvePanel.draw(self, context)
+            ADJT_PT_CurvePanel.draw_ui(self, context, col)
         elif wm.adjt_workflow == 'PLACE':
-            ADJT_PT_AlignPanel.draw(self, context)
+            ADJT_PT_AlignPanel.draw_ui(self, context, col)
         elif wm.adjt_workflow == 'MEASURE':
-            ADJT_PT_MeasurePanel.draw(self, context)
+            ADJT_PT_MeasurePanel.draw_ui(self, context, col)
         elif wm.adjt_workflow == 'TOOLS':
-            ADJT_PT_UtilityPanel.draw(self, context)
+            ADJT_PT_UtilityPanel.draw_ui(self, context, col)
         elif wm.adjt_workflow == 'RENDER':
-            ADJT_PT_RenderPanel.draw(self, context)
+            ADJT_PT_RenderPanel.draw_ui(self, context, col)
+
+
 
 
 def register():
     bat_preview.register()
 
     bpy.types.WindowManager.adjt_workflow = EnumProperty(name='Workflow', items=[
-
         ('MODEL', 'Model', ''),
         ('PLACE', 'Place', ''),
         ('RENDER', 'Render', ''),
